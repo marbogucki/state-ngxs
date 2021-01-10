1d54b7617b243924ddcfc3b38f14684f1c8f8bfb
Marcin Bogucki <mbogucki2@sii.pl>
2021-01-10 19:37:52 +0100

    add decorator to destroy class

--- src/app/shared/helpers/destroy.ts
+++ src/app/shared/helpers/destroy.ts
@@ -1,6 +1,7 @@
-import { OnDestroy } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 
+@Injectable()
 export abstract class Destroy implements OnDestroy {
   protected destroy$: Subject<unknown> = new Subject();
 
